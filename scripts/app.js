b19edf1262abea449628196497d8f37e0091fcf2
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2021-12-04 15:17:18 +0100

    new mechanic

--- scripts/app.js
+++ scripts/app.js
@@ -9,12 +9,33 @@ import { Animation } from "./animations.js";
     const scene = new THREE.Scene();
     //TESTS GO HERE START
     var score = 0;
+
+    //IRON HITBOX
     const geometry = new THREE.BoxGeometry( .7, .5, .5 );
     const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
     const mesh = new THREE.Mesh( geometry, material );
-    mesh.position.set(0,1,2.35)
+    mesh.position.set(0,1,2.35);
     mesh.visible = false;
     scene.add(mesh)
+    //IRON HITBOX
+    
+    //PASSED IRON HITBOX
+    const geometry2 = new THREE.BoxGeometry(.7,.5,.5);
+    const material2 = new THREE.MeshBasicMaterial({color:0xffff0});
+    const mesh2 = new THREE.Mesh(geometry, material);
+    mesh2.position.set(-1.2,1,-0.6);
+    mesh2.visible=false;
+    scene.add(mesh2);
+    //PASSED IRON HITBOX
+    
+    //START HITBOX
+    const geometry3 = new THREE.BoxGeometry(.7,.5,.5);
+    const material3 = new THREE.MeshBasicMaterial({color:0xffff0});
+    const mesh3 = new THREE.Mesh(geometry, material);
+    mesh3.position.set(1.2,1,-0.6);
+    mesh3.visible = false;
+    scene.add(mesh3);
+    //START HITBOX
 
     //TESTS GO HERE END
     
@@ -51,7 +72,6 @@ import { Animation } from "./animations.js";
         - Nerd
         - Skater
         - Muppie
-
         */
     }
 
@@ -59,21 +79,27 @@ import { Animation } from "./animations.js";
     const renderer = new THREE.WebGLRenderer({antialias: true,alpha: true});
     renderer.setSize(375, 812);
     var controls = new OrbitControls( camera, renderer.domElement );
-    var BurgerHitbox = new THREE.Box3().setFromObject(burger);
+    var burgerHitbox = new THREE.Box3().setFromObject(burger);
     var kitchenHitbox = new THREE.Box3().setFromObject(mesh);
+    var passedIronHitbox = new THREE.Box3().setFromObject(mesh2);
+    var startHitbox = new THREE.Box3().setFromObject(mesh3);
     //Tick
-
+    var isExecuted = false;
     const clock = new THREE.Clock();
     const tick = function() {
         controls.update()
         requestAnimationFrame(tick);
         render();
         document.getElementById("points").innerText = score;  
-        if(burger.position.x<0.3 && animate.smashStatus() == false){
-            //console.log("-1 punto");
-        }
-        BurgerHitbox.setFromObject(burger);
+        burgerHitbox.setFromObject(burger);
         kitchenHitbox.setFromObject(mesh);
+        if(burgerHitbox.intersectsBox(passedIronHitbox) && isExecuted==false && animate.smashStatus()==false){
+            console.log("-1 punto");
+            score -=1;
+            isExecuted=true;
+        }else if(burgerHitbox.intersectsBox(startHitbox) && isExecuted == true){
+            isExecuted=false;
+        }
     }
 
     const render = () => {renderer.render(scene,camera)}
@@ -82,7 +108,7 @@ import { Animation } from "./animations.js";
 
 
     renderer.domElement.addEventListener("click", () =>{
-        if(kitchenHitbox.intersectsBox(BurgerHitbox)){
+        if(kitchenHitbox.intersectsBox(burgerHitbox)){
             console.log("hit");
             score+=1;
             animate.setSmash();
