ff93d3ab0c4162eecfbdc673d78f73f1f3333622
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2021-12-04 05:48:08 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -6,12 +6,18 @@ import {loadAssets} from "./load.js";
 import { Animation } from "./animations.js";
 
 (async () => {
-
+    const scene = new THREE.Scene();
     //TESTS GO HERE START
     var score = 0;
+    const geometry = new THREE.BoxGeometry( .8, .5, .5 );
+    const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+    const mesh = new THREE.Mesh( geometry, material );
+    mesh.position.set(.025,1,2.35)
+    mesh.visible = false;
+    scene.add(mesh)
 
     //TESTS GO HERE END
-    const scene = new THREE.Scene();
+    
     const animate = new Animation();
 
 
@@ -66,7 +72,8 @@ import { Animation } from "./animations.js";
     const renderer = new THREE.WebGLRenderer({antialias: true,alpha: true});
     renderer.setSize(375, 812);
     var controls = new OrbitControls( camera, renderer.domElement );
-
+    var BurgerHitbox = new THREE.Box3().setFromObject(burger);
+    var kitchenHitbox = new THREE.Box3().setFromObject(mesh);
     //Tick
 
     const clock = new THREE.Clock();
@@ -75,10 +82,11 @@ import { Animation } from "./animations.js";
         requestAnimationFrame(tick);
         render();
         document.getElementById("points").innerText = score;  
-        
         if(burger.position.x<0.3 && animate.smashStatus() == false){
-            console.log("-1 punto");
+            //console.log("-1 punto");
         }
+        BurgerHitbox.setFromObject(burger);
+        kitchenHitbox.setFromObject(mesh);
     }
 
     const render = () => {renderer.render(scene,camera)}
@@ -87,7 +95,7 @@ import { Animation } from "./animations.js";
 
 
     renderer.domElement.addEventListener("click", () =>{
-        if(burger.position.x >= -0.6 && burger.position.x <= 0.3 ){
+        if(kitchenHitbox.intersectsBox(BurgerHitbox)){
             console.log("hit");
             score+=1;
             animate.setSmash();
@@ -97,6 +105,7 @@ import { Animation } from "./animations.js";
             document.querySelector(".ui-board").style = "z-index:0;display:flex";
             document.getElementById("score").innerText = score;
         }
+        
         
     })  
     document.getElementById("points").innerText = score;
