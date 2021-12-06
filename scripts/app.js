990901e9a648f67c0d7baf9a2157ceb5509d874e
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2021-12-06 19:29:33 +0100

    aaa

--- scripts/app.js
+++ scripts/app.js
@@ -3,96 +3,35 @@ import { GLTFLoader } from "./dependencies/GLTFLoader.js";
 import {OrbitControls} from "./dependencies/OrbitControls.js";
 import TWEEN, { Tween } from "./dependencies/tween.esm.js";
 import {loadAssets} from "./load.js";
-<<<<<<< HEAD
-import { Burger, Skater,Rapper,Muppie, Gamer } from "./animations.js";
+import { Animation, modelSwitcher } from "./animations.js";
 import {mesh,mesh2,mesh3} from "./hitboxes.js";
-=======
-import { Animation } from "./animations.js";
->>>>>>> parent of 8b7db4e (model switching added)
 
 (async () => {
     const scene = new THREE.Scene();
     //TESTS GO HERE START
     var score = 0;
-
-<<<<<<< HEAD
-    //LOAD
     scene.add(mesh,mesh2,mesh3);
-    const {kitchenModel, burgerModel,rapperModel,skaterModel,muppieModel,gamerModel,chefModel} = await loadAssets();
-    const burger = new Burger();
-    const rapper = new Rapper();
-    const skater = new Skater();
-    const muppie = new Muppie();
-    const gamer = new Gamer();
-
-    console.log(burgerModel)
-    burger.model = burgerModel;
-    scene.add(burgerModel);
-    
-
-    //LIGHT
-=======
-    //IRON HITBOX
-    const geometry = new THREE.BoxGeometry( .7, .5, .5 );
-    const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-    const mesh = new THREE.Mesh( geometry, material );
-    mesh.position.set(0,1,2.35);
-    mesh.visible = false;
-    scene.add(mesh)
-    //IRON HITBOX
-    
-    //PASSED IRON HITBOX
-    const geometry2 = new THREE.BoxGeometry(.7,.5,.5);
-    const material2 = new THREE.MeshBasicMaterial({color:0xffff0});
-    const mesh2 = new THREE.Mesh(geometry, material);
-    mesh2.position.set(-1.2,1,-0.6);
-    mesh2.visible=false;
-    scene.add(mesh2);
-    //PASSED IRON HITBOX
-    
-    //START HITBOX
-    const geometry3 = new THREE.BoxGeometry(.7,.5,.5);
-    const material3 = new THREE.MeshBasicMaterial({color:0xffff0});
-    const mesh3 = new THREE.Mesh(geometry, material);
-    mesh3.position.set(1.2,1,-0.6);
-    mesh3.visible = false;
-    scene.add(mesh3);
-    //START HITBOX
-
     //TESTS GO HERE END
-    
     const animate = new Animation();
-
-
     //Loading
 
-    const {kitchen, burger} = await loadAssets();
-    scene.add(kitchen,burger);
+    const {kitchen, burger,rapper,skater,muppie,gamer} = await loadAssets();
+    scene.add(kitchen,burger,rapper,skater,muppie,gamer);
     animate.model = burger;
-    animate.output();
-    console.log(animate.model)
-    animate.startAnimation()
+    //animate.startAnimation()
     //Light
->>>>>>> parent of 8b7db4e (model switching added)
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
     scene.add(ambientLight);
     const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
     directionalLight.position.set(4,4,0)
     scene.add(directionalLight);
-    //LIGHT
 
-    //CAMERA
+    //Camera
     const width = 4;
     const height = width * (812/375);
     const camera = new THREE.OrthographicCamera(width / -2,width / 2,height / 2,height / -2, 1, 100);
     camera.position.set(.2,2,4);
     camera.lookAt(0,0,0);
-<<<<<<< HEAD
-    //CAMERA
-=======
->>>>>>> parent of 8b7db4e (model switching added)
-
-
     //Main game
     function gameOn(){
         /* 
@@ -103,21 +42,13 @@ import { Animation } from "./animations.js";
         */
     }
 
-
     //Renderer
-<<<<<<< HEAD
-    
+    animate.startAnimation()
     var currentModel = burger;
     const renderer = new THREE.WebGLRenderer({antialias: true,alpha: true});
     renderer.setSize(375, 812);
-    //var controls = new OrbitControls( camera, renderer.domElement );
-    var burgerHitbox = new THREE.Box3().setFromObject(currentModel);
-=======
-    const renderer = new THREE.WebGLRenderer({antialias: true,alpha: true});
-    renderer.setSize(375, 812);
     var controls = new OrbitControls( camera, renderer.domElement );
-    var burgerHitbox = new THREE.Box3().setFromObject(burger);
->>>>>>> parent of 8b7db4e (model switching added)
+    var burgerHitbox = new THREE.Box3().setFromObject(currentModel);
     var kitchenHitbox = new THREE.Box3().setFromObject(mesh);
     var passedIronHitbox = new THREE.Box3().setFromObject(mesh2);
     var startHitbox = new THREE.Box3().setFromObject(mesh3);
@@ -125,24 +56,23 @@ import { Animation } from "./animations.js";
     var isExecuted = false;
     const clock = new THREE.Clock();
     const tick = function() {
-        //controls.update()
+        controls.update()
         requestAnimationFrame(tick);
         render();
         document.getElementById("points").innerText = score;  
-        burgerHitbox.setFromObject(burger);
+        burgerHitbox.setFromObject(currentModel);
         kitchenHitbox.setFromObject(mesh);
         if(burgerHitbox.intersectsBox(passedIronHitbox) && isExecuted==false && animate.smashStatus()==false){
-            //score -=1;
+            score -=1;
             isExecuted=true;
         }else if(burgerHitbox.intersectsBox(startHitbox) && isExecuted == true){
             isExecuted=false;
-<<<<<<< HEAD
+            currentModel = animate.switchModel(scene.getObjectByName(modelSwitcher()));
             burgerHitbox.setFromObject(currentModel);
-            //console.log(currentModel);
+            console.log(currentModel);
+            animate.startAnimation();
         }else if(burgerHitbox.intersectsBox(passedIronHitbox) && isExecuted==false){
             isExecuted=true;        
-=======
->>>>>>> parent of 8b7db4e (model switching added)
         }
     }
 
@@ -152,13 +82,12 @@ import { Animation } from "./animations.js";
 
 
     renderer.domElement.addEventListener("click", () =>{
-<<<<<<< HEAD
-        if(0){
-=======
         if(kitchenHitbox.intersectsBox(burgerHitbox)){
->>>>>>> parent of 8b7db4e (model switching added)
+            
             score+=1;
+            animate.setSmash();
         }else{
+            animate.unsetSmash();
             console.log("no hit");
             document.querySelector(".ui-board").style = "z-index:0;display:flex";
             document.getElementById("score").innerText = score;
--- scripts/load.js
+++ scripts/load.js
@@ -7,72 +7,51 @@ const loader = new GLTFLoader();
 
 
 export async function loadAssets(){
-<<<<<<< HEAD
-    const [kitchenData, burgerData,rapperData,skaterData,muppieData,gamerData,chefData] = await Promise.all([
-=======
-    const [kitchenData, burgerData,trapperData,skaterData,muppieData,gamerData] = await Promise.all([
->>>>>>> parent of 8b7db4e (model switching added)
+    const [kitchenData, burgerData,rapperData,skaterData,muppieData,gamerData] = await Promise.all([
         loader.loadAsync("./assets/kitchen.glb"),
         loader.loadAsync("./assets/burger.glb"),
         loader.loadAsync("./assets/rapper.glb"),
         loader.loadAsync("./assets/skater.glb"),
         loader.loadAsync("./assets/muppie.glb"),
-<<<<<<< HEAD
         loader.loadAsync("./assets/gamer.glb"),
-        loader.loadAsync("./assets/chef.glb")
-=======
-        //loader.loadAsync("./assets/nerd.glb"),
->>>>>>> parent of 8b7db4e (model switching added)
     ]);
 
-    const kitchenModel = kitchenData.scene;
-    kitchenModel.scale.set(3, 3, 3);
-    kitchenModel.name = "kitchen";
+    const kitchen = kitchenData.scene;
+    kitchen.scale.set(3, 3, 3);
+    kitchen.name = "kitchen";
 
-    const burgerModel = burgerData.scene;
-    burgerModel.name = "burger";
-    burgerModel.scale.set(.25, .25, .25);
-    burgerModel.position.set(1,0,-.6);
+    const burger = burgerData.scene;
+    burger.name = "burger";
+    burger.scale.set(.3, .3, .3);
+    burger.position.set(1,0,-.6);
 
-<<<<<<< HEAD
-    const rapperModel = rapperData.scene;
-    rapperModel.name = "rapper";
-    rapperModel.scale.set(.25, .25, .25);
-    rapperModel.position.set(1.12,0,-.6);
+    const rapper = rapperData.scene;
+    rapper.name = "rapper";
+    rapper.scale.set(.3, .3, .3);
+    rapper.position.set(1.12,0,-.6);
 
-    const skaterModel = skaterData.scene;
-    skaterModel.name = "skater";
-    skaterModel.scale.set(.25, .25, .25);
-    skaterModel.position.set(1.12,0,-.6);
+    const skater = skaterData.scene;
+    skater.name = "skater";
+    skater.scale.set(.3, .3, .3);
+    skater.position.set(1.12,0,-.6);
 
-    const muppieModel = muppieData.scene;
-    muppieModel.name = "muppie";
-    muppieModel.scale.set(.25, .25, .25);
-    muppieModel.position.set(1.12,0,-.6);
+    const muppie = muppieData.scene;
+    muppie.name = "muppie";
+    muppie.scale.set(.3, .3, .3);
+    muppie.position.set(1.12,0,-.6);
 
-    const gamerModel = gamerData.scene;
-    gamerModel.name = "gamer";
-    gamerModel.scale.set(.25, .25, .25);
-    gamerModel.position.set(1.12,0,-.6);
+    const gamer = gamerData.scene;
+    gamer.name = "gamer";
+    gamer.scale.set(.3, .3, .3);
+    gamer.position.set(1.12,0,-.6);
 
-    const chefModel = chefData.scene;
-    chefModel.name = "chef";
-    chefModel.scale.set(1,1,1);
-    chefModel.position.set(-.5,0,1.2);
-
-    return{
-        kitchenModel,
-        burgerModel,
-        rapperModel,
-        skaterModel,
-        muppieModel,
-        gamerModel,
-        chefModel
-=======
     return{
         kitchen,
-        burger
->>>>>>> parent of 8b7db4e (model switching added)
+        burger,
+        rapper,
+        skater,
+        muppie,
+        gamer
     }
     
 }
