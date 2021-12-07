73a7f53c452d2256d44fc10245b56327e5090c04
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2021-12-07 04:13:31 +0100

    xd

--- recode_b/src/World/core.js
+++ recode_b/src/World/core.js
@@ -9,6 +9,6 @@ const geometry = new THREE.BoxGeometry( .02, .2, .2 );
 const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 export const mesh = new THREE.Mesh( geometry, material );
 mesh.position.set(0,.3,.77);
-
+mesh.visible = false;
 export const ironHitbox = new THREE.Box3().setFromObject(mesh)
 
